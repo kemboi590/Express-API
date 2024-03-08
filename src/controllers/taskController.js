4a0cecf9ec0870fc491843c16c5f15e43fe78fdf
kemboi590 <bkemboi590@gmail.com>
2024-03-08 23:11:11 +0300

    node API two

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,18 +1,68 @@
-import sql from "mssql"
-import config from './../db/config.js';
+import sql from "mssql";
+import config from "./../db/config.js";
 
-export const CreateTodo = async(req, res) => {
-    try {
-        const { title, description } = req.body;
-        const pool = await sql.connect(config.sql);
-        await pool
-        .request()
-        .input("title", sql.VarChar, title)
-        .input("description", sql.VarChar, description)
-        .query("INSERT INTO todo (title, description) VALUES (@title, @description)");
-        res.json("Todo Created successfully");
-        
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+//CRUD - CREATE, READ, UPDATE, DELETE.
+
+//READ
+export const GetTodos = async (req, res) => {
+  try {
+    const pool = await sql.connect(config.sql);
+    const result = await pool.request().query("SELECT * FROM todo");
+    res.status(200).json(result.recordset);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//CREATE
+export const CreateTodo = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const pool = await sql.connect(config.sql);
+    await pool
+      .request()
+      .input("title", sql.VarChar, title)
+      .input("description", sql.VarChar, description)
+      .query(
+        "INSERT INTO todo (title, description) VALUES (@title, @description)"
+      );
+    res.json("Todo Created successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//UPDATE
+export const UpdateTodo = async (req, res) => {
+  try {
+    const { id } = req.params;  //2
+    const { title, description } = req.body;
+    const pool = await sql.connect(config.sql);
+    await pool
+      .request()
+      .input("id", sql.VarChar, id)
+      .input("title", sql.VarChar, title)
+      .input("description", sql.VarChar, description)
+      .query(
+        "UPDATE todo SET title = @title, description = @description WHERE todo_id = @id"
+      );
+    res.status(200).json("Todo updated successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//DELETE
+export const DeleteTodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const pool = await sql.connect(config.sql);
+    await pool
+      .request()
+      .input("id", sql.Int, id)
+      .query("DELETE FROM todo WHERE todo_id = @id");
+    res.json("Todo Deleted successfully");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- src/routes/todo.js
+++ src/routes/todo.js
@@ -1,7 +1,14 @@
-import { CreateTodo } from "../controllers/taskController.js";
+import {
+  CreateTodo,
+  GetTodos,
+  DeleteTodo,
+  UpdateTodo,
+} from "../controllers/taskController.js";
 
+//http://localhost:8082/todo
 const tasks = (app) => {
-  app.post("/todo", CreateTodo);
+  app.post("/todo", CreateTodo).get("/todo", GetTodos);
+  app.delete("/todo/:id", DeleteTodo).put("/todo/:id", UpdateTodo);
 };
 
 export default tasks;
